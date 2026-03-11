67bdb483b6d1997c51a7dc25b7c4fad574832ff6
Mariana Dutra <mariana.67005@aluno.iffar.edu.br>
2026-03-11 11:23:58 -0300

    Add files via upload

---
+++ agenda.30/conexao.php
@@ -0,0 +1,11 @@
+<?php
+$SERVIDOR = "localhost";
+$USUARIO = "root";
+$SENHA = "";
+$BANCO = "agenda_30";
+$CONEXAO = mysqli_connect($SERVIDOR, $USUARIO, $SENHA, $BANCO);
+
+if (!$CONEXAO){
+    die ("Erro de conexão. ".mysqli_connect_error());
+}
+?>
---
+++ agenda.30/index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <title>Agenda de Contatos - T30</title>
+</head>
+<body>
+    <h1>Agenda - Turma 30 - 2026</h1>
+    <h2>Cadastrar contatos</h2>
+
+    <form action="salvar.php" method="post">
+    Nome: <input type="text" name="NOME"><br><br>
+    Endereço: <input type="text" name="ENDERECO"><br><br>
+    Telefone: <input type="text" name="TELEFONE"><br><br>
+    <input type="submit" value="CADASTRAR">
+    </form>
+
+<?php 
+include ('conexao.php');
+$SQL = "SELECT * FROM CONTATOS";
+$RESULTADO = mysqli_query($CONEXAO, $SQL);
+
+if (mysqli_num_rows($RESULTADO) > 0){
+    while ($LINHA = mysqli_fetch_assoc($RESULTADO)){
+        echo $LINHA['nome'] . " | " . $LINHA['endereco'] . " | " . $LINHA['telefone'] . " | <a href='editar.php?id=" . $LINHA['id'] . "'>Editar</a> | <a href='excluir.php?id='>Excluir</a>" . "<br>";
+    }
+} else {
+    echo "<h3>Nenhum contato registrado.</h3>";
+}
+?>
+
+</body>
+</html>

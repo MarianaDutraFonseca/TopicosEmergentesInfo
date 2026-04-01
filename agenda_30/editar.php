834f26b58ffb884dd86989954257d0935044005c
Mariana Dutra <mariana.67005@aluno.iffar.edu.br>
2026-04-01 10:18:23 -0300

    Add files via upload

---
+++ agenda_30/editar.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+    <title>Editar</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+
+<header><a href="https://www.iffarroupilha.edu.br/" target="_blank"><img src="iff.png" class="iff"></a></header>
+
+    <div class="inicio">
+    <div>
+        <div class="text-center">
+            <h1>Turma 30 INFO 2026</h1>
+            <p>Agenda - Setor de Contatos</p>
+        </div>
+    </div></div>
+
+    <div class="AH">
+<?php
+include('conexao.php');
+$id = $_GET['id'];
+
+$SQL = "SELECT * FROM contatos WHERE id = $id";
+$RESULTADO = mysqli_query($CONEXAO, $SQL);
+if (mysqli_num_rows($RESULTADO) == 1){
+    $CONTATO = mysqli_fetch_assoc($RESULTADO);
+} else {
+    echo "Contato não encontrado a base.";
+    exit;
+}
+
+if (isset($_POST['Atualizar'])){
+    $NOVO_NOME = $_POST['Nome'];
+    $NOVO_ENDERECO = $_POST['Endereco'];
+    $NOVO_TELEFONE = $_POST['Telefone'];
+
+    $SQL2 = "UPDATE contatos SET nome = '$NOVO_NOME',
+        endereco = '$NOVO_ENDERECO', telefone = '$NOVO_TELEFONE'
+        WHERE id = $id";
+
+    if (mysqli_query($CONEXAO, $SQL2)){
+        echo "<div class='sucesso'>Contato atualizado com sucesso!</div>";
+        echo "<a href='index.php' class='voltar'>VOLTAR</a>";
+        exit;
+    } else {
+        echo "<div class='erro'>Erro ao atualizar.</div>" . mysqli_error($CONEXAO);
+        echo "<a href='index.php' class='voltar'>VOLTAR</a>";
+        exit; 
+    }
+}
+?></div>
+
+    <div class="formulario">
+        <form method="post">
+        <label for="nome" class="form-label">Nome:</label>
+            <input type="text" class="form-control" name="Nome" required value="<?php echo $CONTATO['nome']; ?>">
+
+        <label for="endereco" class="form-label">Endereço:</label>
+            <input type="text" class="form-control" name="Endereco" required value="<?php echo $CONTATO['endereco']; ?>"><br>
+
+        <label for="telefone" class="form-label">Telefone:</label>
+            <input type="text" class="form-control" name="Telefone" required onkeyup= "mascara(this)" value="<?php echo $CONTATO['telefone']; ?>"><br>
+            <input type="submit" class="btn-light" name="Atualizar" value="ATUALIZAR">
+        </form>
+    </div><br><br>
+
+<script>
+function mascara(campo){
+    let valor = campo.value.replace(/\D/g, "");
+
+    if(valor.length > 10){
+        valor = valor.replace(/^(\d{2})(\d{5})(\d{4}).*/, "($1) $2-$3");
+    }else{
+        valor = valor.replace(/^(\d{2})(\d{4})(\d{4}).*/, "($1) $2-$3");
+    }
+    campo.value = valor;
+}
+</script>
+    
+<footer>© 2026 IFFar - Todos os direitos reservados.</footer>
+</body>
+</html>
---
+++ agenda_30/index.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+    <title>Agenda de Contatos - T30</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+
+<header><a href="https://www.iffarroupilha.edu.br/" target="_blank"><img src="iff.png" class="iff"></a></header>
+
+    <div class="inicio">
+    <div>
+        <div class="text-center">
+            <h1>Turma 30 INFO 2026</h1>
+            <p>Agenda - Setor de Contatos</p>
+        </div>
+    </div></div>
+
+    <div class="tudo">
+    <div class="formulario">
+    <div>
+            <form action="salvar.php" method="post">
+                <label for="nome" class="form-label">Nome:</label>
+                    <input type="text" class="form-control" name="NOME" required>
+
+                <label for="endereco" class="form-label">Endereço:</label>
+                    <input type="text" class="form-control" name="ENDERECO" required><br>
+
+                <label for="telefone" class="form-label">Telefone:</label>
+                    <input type="text" class="form-control" name="TELEFONE" maxlength="15" onkeyup="mascaraTelefone(this)" required><br>
+                    <input type="submit" class="btn-light" value="CADASTRAR">
+            </form>
+    </div></div>
+
+    <div class="php">
+        <div>
+        <?php 
+        include ('conexao.php');
+        $SQL = "SELECT * FROM CONTATOS";
+        $RESULTADO = mysqli_query($CONEXAO, $SQL);
+
+        if (mysqli_num_rows($RESULTADO) > 0){
+            while ($LINHA = mysqli_fetch_assoc($RESULTADO)){
+                echo $LINHA['nome'] . " | " . 
+                $LINHA['endereco'] . " | " . 
+                $LINHA['telefone'] . " | 
+
+                <a href='editar.php?id=" . $LINHA['id'] . "'>
+                <img src='editar.png' width='20'></a> 
+
+                <a href='excluir.php?id=" . $LINHA['id'] . "'
+                onclick='return confirm(\"Você quer realmente excluir o contato?\");'>
+                <img src='excluir.png' width='20'></a><br>";
+            }
+        } else {
+            echo "<h3>Nenhum contato registrado.</h3>";
+        }
+        ?>
+    </div></div></div><br><br>
+
+<script>
+function mascaraTelefone(campo){
+    let valor = campo.value.replace(/\D/g, "");
+
+    if(valor.length > 10){
+        valor = valor.replace(/^(\d{2})(\d{5})(\d{4}).*/, "($1) $2-$3");
+    }else{
+        valor = valor.replace(/^(\d{2})(\d{4})(\d{4}).*/, "($1) $2-$3");
+    }
+    campo.value = valor;
+}
+</script>
+
+<footer>© 2026 IFFar - Todos os direitos reservados.</footer>
+</body>
+</html>
